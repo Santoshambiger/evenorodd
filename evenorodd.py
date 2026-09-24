7935f5672778101ea944d1d1f3fd468703beeae7
santosh ambiger <ambigersantosh79@gmail.com>
2026-09-24 21:13:48 +0530

    updated the greeting

--- evenorodd.py
+++ evenorodd.py
@@ -1,6 +1,8 @@
 num = int(input("Enter a number: "))
 
-if num % 2 == 0:
-    print("Even")
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
 else:
-    print("Odd")
+    print("Zero")
